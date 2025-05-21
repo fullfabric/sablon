09a285b182fa8a738c85f99f5ccff9b5e2200a5b
Jack Millard <millard64@hotmail.co.uk>
2025-05-21 15:04:37 +0100

    Update merge field pattern to allow for more whitespace

--- lib/sablon/parser/mail_merge.rb
+++ lib/sablon/parser/mail_merge.rb
@@ -2,33 +2,34 @@ module Sablon
   module Parser
     class MailMerge
       class MergeField
-        KEY_PATTERN = /^\s*MERGEFIELD\s+([^ ]+)\s+\\\*\s+MERGEFORMAT\s*$/
+        KEY_PATTERN = /^\s*MERGEFIELD\s+(.+?)\s+\\\*\s+MERGEFORMAT\s*$/
 
         def valid?
           expression
         end
 
         def expression
-          $1 if @raw_expression =~ KEY_PATTERN
+          ::Regexp.last_match(1) if @raw_expression =~ KEY_PATTERN
         end
 
         private
+
         def replace_field_display(node, content)
-          paragraph = node.ancestors(".//w:p").first
+          paragraph = node.ancestors('.//w:p').first
           display_node = get_display_node(node)
           content.append_to(paragraph, display_node)
           display_node.remove
         end
 
         def get_display_node(node)
-          node.search(".//w:t").first
+          node.search('.//w:t').first
         end
       end
 
       class ComplexField < MergeField
         def initialize(nodes)
           @nodes = nodes
-          @raw_expression = @nodes.flat_map {|n| n.search(".//w:instrText").map(&:content) }.join
+          @raw_expression = @nodes.flat_map { |n| n.search('.//w:instrText').map(&:content) }.join
         end
 
         def valid?
@@ -57,19 +58,20 @@ module Sablon
         end
 
         private
+
         def pattern_node
           separate_node.next_element
         end
 
         def separate_node
-          @nodes.detect {|n| !n.search(".//w:fldChar[@w:fldCharType='separate']").empty? }
+          @nodes.detect { |n| !n.search(".//w:fldChar[@w:fldCharType='separate']").empty? }
         end
       end
 
       class SimpleField < MergeField
         def initialize(node)
           @node = node
-          @raw_expression = @node["w:instr"]
+          @raw_expression = @node['w:instr']
         end
 
         def replace(content)
@@ -89,20 +91,21 @@ module Sablon
         def start_node
           @node
         end
-        alias_method :end_node, :start_node
+        alias end_node start_node
 
         private
+
         def remove_extra_runs!
-          @node.search(".//w:r")[1..-1].each(&:remove)
+          @node.search('.//w:r')[1..-1].each(&:remove)
         end
       end
 
       def parse_fields(xml)
         fields = []
         xml.traverse do |node|
-          if node.name == "fldSimple"
+          if node.name == 'fldSimple'
             field = SimpleField.new(node)
-          elsif node.name == "fldChar" && node["w:fldCharType"] == "begin"
+          elsif node.name == 'fldChar' && node['w:fldCharType'] == 'begin'
             field = build_complex_field(node)
           end
           fields << field if field && field.valid?
